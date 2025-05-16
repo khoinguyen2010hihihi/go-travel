6b91ee49ff45e6357a6780cc5253d9ebfb33ae0f
Nguyen Khoi <khoibene@gmail.com>
2025-05-16 21:02:10 +0700

    temp of temp

--- homestayweb/src/main/java/com/homestay/homestayweb/service/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.homestay.homestayweb.service;
-
-import com.homestay.homestayweb.dto.response.PaymentResponse;
-import jakarta.servlet.http.HttpServletRequest;
-
-public interface PaymentService {
-    PaymentResponse.VNPayResponse createVnPayPayment(HttpServletRequest request);
-}
